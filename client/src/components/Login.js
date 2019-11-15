dd716c4406110dc30ab52c272b915e1d9efc44d3
Darrin Lowery <lambda@monsto.com>
2019-11-15 10:35:56 -0600

    login: basic plus.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "http://localhost:5000/api";
 
 function clg(...x) {
 	for (let exes of x) console.log(exes);
@@ -19,15 +19,29 @@ const Login = () => {
 		setCred({ ...cred, [e.target.name]: e.target.value })
 	}
 
+	const loginAction = e => {
+		e.preventDefault();
+		axios
+			.post(`${url}/login`, cred)
+			.then(res => {
+				clg(res.data)
+				sessionStorage.setItem("token", res.data.payload)
+				setIsLoggedIn(true);
+				// props.history.push("/friendlist");
+			})
+			.catch(err => clg(err))
+	}
 
 	// make a post request to retrieve a token from the api
 	// when you have handled the token, navigate to the BubblePage route
+
+	// username === "Lambda School" && 
+	const pass = "i<3Lambd4";
+
 	return (
 		<>
 			<div>
 				<h1>Welcome to the Bubble App!</h1>
-			</div>
-			<div>
 				<p>
 					{isLoggedIn ? "Logged In" : "Do a logging in plz"}
 				</p>
